b851f449f6564ba4e601e1fd7680ede490c88c0b
Karan Banjade <karan.banjade@gmail.com>
2022-06-15 13:04:34 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
 import Login from './Components/Login/Login'
-
 function App() {
   return (
-   <div>
-      <Login />
-   </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path = "/">
+          <Route index element = {<Login/>}/>
+          <Route path ="Test" element = {<div><h1>Test</h1></div>}/>
+        </Route>
+      </Routes>
+   </BrowserRouter>
   );
 }
 
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,10 +5,13 @@ import './Login.css';
 // Importing Axios for hitting APIs
 import axios from 'axios';
 
+import { Outlet, Link } from "react-router-dom";
+
 const Login = () => {
     const [input,setInput] = useState('');
     const [password,setPassword] = useState('');
-    const loginApiFunction = () => {
+    const loginApiFunction = (e) => {
+        e.preventDefault();
         let data = {
                 "password" : password
             }
@@ -17,15 +20,16 @@ const Login = () => {
         else
             data["username"] = input
         axios({
-			method: 'post',
+			method: 'POST',
 			url: `http://localhost:5000/user/login`,
 			data,
 		})
-        .then(response => JSON.stringify(response))
         .then((data)=>{
+            console.log(data);
             alert(data);
         })
         .catch((err) => {
+            console.log(err);
             alert(err);
         });
     }
@@ -37,13 +41,18 @@ const Login = () => {
                     <form id = "input-div" autocomplete = "off" >
                         <input type="text" placeholder="Username/Mail" className="login-input" value={input} onChange={(e)=>{setInput(e.target.value)}}/>    
                         <input type="password" placeholder="Password" className="login-input" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-                        <button onClick={() => loginApiFunction()} id = "login-button">Login</button>
+                        <button onClick={(e) => loginApiFunction(e)} id = "login-button">Login</button>
+                        <container id="register-container"> 
+                            <span class="material-symbols-outlined">how_to_reg</span>
+                            <Link to="/test" id = "signup-link">
+                                No Account? Register Now!
+                            </Link>
+                        </container>
                     </form>
                 </div>
                 <span>
                     <img src = "Images/sideImg.png" alt="img" width="300rem" id='login-pencil' />
                 </span>
-
             </center>
         </div>
     )
