f1b0e373963c27eed19026bb695c22e500b198c9
Karan Banjade <karan.banjade@gmail.com>
2022-06-29 21:35:35 +0530

    error page fixed

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import "./ErrorPage.css";
 const ErrorPage = (props) => {
     return (
-        <div className="error-page">
-            <h1>Error</h1>
-            <p>{props.error || "Something Went Wrong"}</p>
+        <div id="error-page-container">
+            <div id="error-page-content">
+                <div className="error-page">
+                    <h1>Error</h1>
+                    <p>{props.error || "Something Went Wrong"}</p>
+                </div>
+                <Link style={{textDecoration: 'none'}}  to = "/">
+                    <button className = "error-page-buttons">Login Again</button>
+                </Link>
+                <button className = "error-page-buttons" onClick = {()=>location.reload()}>Try Again</button>
+            </div>
         </div>
     )
 }
